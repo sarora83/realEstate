6d679ad0ee44dc6cbe3a9cb74dbce56e1f385527
sarora83 <arorasagar83@gmail.com>
2023-11-07 21:31:08 +0530

    Create A User API route and controller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js"
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB)
@@ -16,3 +17,5 @@ const app = express()
 app.listen(3000, ()=> {
     console.log("Server is listening at 3000");
 })
+
+app.use("/api/user", userRouter);
