e589761b300a36e578f8b89f70127ada60f19e1c
sarora83 <arorasagar83@gmail.com>
2023-11-07 16:55:43 +0530

    created mongoose connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB)
+.then(() => {
+    console.log("Connected");
+})
+.catch((error) => {
+    console.log(error.message);
+});
 
 const app = express()
 
