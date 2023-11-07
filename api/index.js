3fbddb5cdc45d7c17a10f86456503895cd269cda
sarora83 <arorasagar83@gmail.com>
2023-11-07 22:27:02 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB)
@@ -12,10 +14,13 @@ mongoose.connect(process.env.MONGODB)
     console.log(error.message);
 });
 
-const app = express()
+const app = express();
+app.use(express.json());
+
 
 app.listen(3000, ()=> {
     console.log("Server is listening at 3000");
 })
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
