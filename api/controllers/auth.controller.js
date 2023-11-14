d1442a2aba7f611ae74724c2d007032f01485eb3
sarora83 <arorasagar83@gmail.com>
2023-11-14 22:17:08 +0530

    complete sign up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,19 @@
+import bcrypt from "bcryptjs/dist/bcrypt.js";
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs"
+
 
 export const signup = async (req, res, next)=> {
 
     const {username, email, password} = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 12);
+    //const salt = await bcrypt.genSaltSync(10);
+    //req.body.password = await bcrypt.hash(req.body.password, salt);
+    //const salt = bcryptjs.genSaltSync(10)
 
-    const newUser = new User({username, email, password: hashedPassword});
+    var salt =  bcrypt.genSaltSync(10);
+    const hashedPassword =  bcrypt.hashSync(req.body.password, salt);
+    //const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const newUser = new User({username, email, password : hashedPassword});
     try{
         await newUser.save();
         res.status(201).json("User created");
--- realEstate/src/pages/SignUp.jsx
+++ realEstate/src/pages/SignUp.jsx
@@ -1,15 +1,68 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link , useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+  //getting data from sign up form
+  const [formData, setFormData] = useState({});
+
+  const [error, setError] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  //handle change in form data
+  const handleChange = (e) => {
+    setFormData (
+      {
+        ...formData,
+        [e.target.id] : e.target.value,
+      }
+    );
+  };
+
+//Handle submit button
+  const  handleSubmit = async (e) =>  {
+    try {
+    e.preventDefault();
+    setLoading(true)
+    const res = await fetch('/api/auth/signup', 
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }
+    );
+    const data = await res.json();
+    if (data.success === false) {
+      setError(data.message)
+      setLoading(false);
+
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    navigate('/sign-in')
+    console.log(data);
+  }
+  catch(error){
+    setLoading(false);
+    setError(data.message)
+  }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4 '>
-        <input type ='text' placeholder='username'className='border p-3 rounded-lg' id= 'username'></input>
-        <input type ='email' placeholder='email'className='border p-3 rounded-lg' id= 'email'></input>
-        <input type ='password' placeholder='password'className='border p-3 rounded-lg' id= 'password'></input>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-50'>Sign Up</button>
+      <form onSubmit= {handleSubmit} className='flex flex-col gap-4 '>
+        <input type ='text' placeholder='username' className='border p-3 rounded-lg' id= 'username' onChange={handleChange}></input>
+        <input type ='email' placeholder='email' className='border p-3 rounded-lg' id= 'email' onChange={handleChange}></input>
+        <input type ='password' placeholder='password'className='border p-3 rounded-lg' id= 'password' onChange={handleChange}></input>
+        <button disabled ={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-50'>
+          
+          {loading ? 'Loading ...' : 'Sign Up'}
+          </button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
@@ -17,6 +70,8 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-700 mt-5'>{error}</p>}
+      <div></div>
     </div>
   )
 }
