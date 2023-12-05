a3a1b67224716ec58b8847d5ca219ba512228456
sarora83 <arorasagar83@gmail.com>
2023-12-05 10:42:46 +0530

    Sigin API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs/dist/bcrypt.js";
 import User from "../models/user.model.js";
-
+import {errorHandler} from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next)=> {
 
@@ -17,6 +18,33 @@ export const signup = async (req, res, next)=> {
     try{
         await newUser.save();
         res.status(201).json("User created");
+
+    } catch(error) {
+        next(error);
+    }
+
+};
+export const signin = async (req, res, next)=> {
+
+    const { email, password} = req.body;
+    
+    try{
+
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+
+            return next(errorHandler(404, "User not found"));
+        }
+
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if(!validPassword) { return next(errorHandler(401, "Wrong Credentials"))}
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET )
+        const {password: pass, ...rest}  = validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true, expires: new Date(Date.now() + 24 *60 *60 * 1000)})
+        .status(200)
+        .json(rest)
+     
+        
 
     } catch(error) {
         next(error);
